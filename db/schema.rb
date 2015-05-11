c2154f616770f68b91d8351309569ef236295e53
Frank Johnson & Kim Dang <student@epicodus.com>
2015-05-11 12:18:07 -0700

    add list.rb and passing test

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150511163806) do
+ActiveRecord::Schema.define(version: 20150511190715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lists", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string   "description"
@@ -22,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150511163806) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "due_date"
+    t.integer  "list_id"
   end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require 'rspec'
 require 'pg'
 require 'sinatra/activerecord'
 require 'task'
+require 'list'
 
 RSpec.configure do |config|
   config.after(:each) do
